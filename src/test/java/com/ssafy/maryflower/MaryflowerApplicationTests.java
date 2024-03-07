3c386ea2a01822c1d4a23607a555642218c5fd8f
우창진 <deu03161@naver.com>
2024-03-07 12:54:41 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/ssafy/maryflower/MaryflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.maryflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaryflowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
