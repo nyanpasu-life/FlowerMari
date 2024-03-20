3fa62958f7744beb1fbd3e8ce7fccb858b1a833e
우창진 <deu03161@naver.com>
2024-03-20 16:10:22 +0900

    195 : Main Flow 구현 후 머지

---
+++ backend/src/main/java/com/ssafy/maryflower/bouquet/controller/BouquetController.java
@@ -0,0 +1,54 @@
+package com.ssafy.maryflower.bouquet.controller;
+
+import com.ssafy.maryflower.bouquet.data.dto.request.UserDataHolder;
+import com.ssafy.maryflower.bouquet.service.BouquetService;
+import com.ssafy.maryflower.bouquet.service.CacheService;
+import com.ssafy.maryflower.bouquet.service.DataPublishService;
+import com.ssafy.maryflower.bouquet.sse.SseEmitters;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequestMapping("/bouquet")
+@RequiredArgsConstructor
+public class BouquetController {
+
+    private final SseEmitters sseEmitters;
+    private final BouquetService bouquetService;
+    private final CacheService cacheService;
+    private final DataPublishService DataPublishService;
+
+    @GetMapping("/text-input")
+    public SseEmitter processSendUserInputToAIServer(@RequestBody UserDataHolder userDataHolder){
+
+        // 토큰에서 userId 추출.
+        Long userId=1L;
+
+        // api 호출 회수 조회.
+        if(bouquetService.checkApiUses(userId)>5) {
+            throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS,"API 사용 횟수를 초과하였습니다");
+        }
+
+        // api 사용로그 저장
+        bouquetService.createApiLog(userId);
+
+        // 요청에 대한 requestId 생성
+        String requestId= bouquetService.generateRequestID();
+
+        // userData를 담아 놓을 Dto 생성
+        userDataHolder.setUserId (userId);
+
+        // userID를 key로 하여 데이터 Redis 캐시에 저장.
+        cacheService.cacheUserDataWithUserId(requestId, userDataHolder);
+
+        // API를 통해 꽃다발에 사용할 꽃 추출 후, Redis ch1으로 publish
+        DataPublishService.publishFlowerDataToAIServer(userDataHolder.getWhom(),userDataHolder.getSituation(),userDataHolder.getMessage(),requestId);
+
+        // sseEmitter 생성 후 반환
+        return sseEmitters.addEmitter(requestId);
+    }
+
+}
---
+++ backend/src/main/java/com/ssafy/maryflower/bouquet/sse/SseEmitters.java
@@ -0,0 +1,49 @@
+package com.ssafy.maryflower.bouquet.sse;
+
+import com.ssafy.maryflower.bouquet.data.dto.response.firstGenerateDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class SseEmitters {
+
+    // requestId를 키로 하고, SseEmitter를 값으로 하는 Map
+    private final Map<String, SseEmitter> emitters=new ConcurrentHashMap<>();
+
+    // 클라이언트와 연결 추가.
+    public SseEmitter addEmitter(String requestId){
+        SseEmitter emitter=new SseEmitter(Long.MAX_VALUE);
+        emitters.put(requestId, emitter);
+
+        //클라이언트와 연결 종료 시 이벤트 핸들러
+        emitter.onCompletion(() -> emitters.remove(requestId));
+        emitter.onTimeout(() -> emitters.remove(requestId));
+        emitter.onError((e) -> emitters.remove(requestId));
+
+        return emitter;
+    }
+
+    // 연결을 제거
+    public void removeEmitter(String requestId) {
+        emitters.remove(requestId);
+    }
+
+    // requestId에 따라 해당 클라이언트에게 메세지 전송.
+    public void sendfirstGenerateDtoToClient(String requestId,firstGenerateDto firstGenerateDto){
+        SseEmitter emitter= emitters.get(requestId);
+        if(emitter !=null){
+            try{
+                emitter.send(SseEmitter.event().name("firstGenerateEvent").data(firstGenerateDto));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // sse를 통해 연결되어 있는 클라이언트에게 메세지를 보냄.
+
+}
