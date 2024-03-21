6873e6335c873cc438b2f655f152b5bf07f28255
unknown <ygj9605@naver.com>
2024-03-21 17:43:32 +0900

    Feat: axios 간이 설정 추가

--- frontend/src/api/bouquetCreate.ts
+++ frontend/src/api/bouquetCreate.ts
@@ -5,10 +5,12 @@ interface TextInputs {
     situation: string;
     message: string;
 }
-
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+});
 export const postTextInputs = async (inputs: TextInputs): Promise<void> => {
     try {
-        const response = await axios.post('/bouquet/text-input', inputs);
+        const response = await api.post('/bouquet/text-input', inputs);
         console.log(response.data);
     } catch (error) {
         console.error(error);
