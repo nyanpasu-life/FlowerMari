0478a73c50ade30a13f0257e359e47bcc68fd19d
우창진 <deu03161@naver.com>
2024-03-31 20:09:23 +0900

    HotFix : console log 추가.

--- backend/src/main/java/com/ssafy/maryflower/global/util/MemberUtil.java
+++ backend/src/main/java/com/ssafy/maryflower/global/util/MemberUtil.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class MemberUtil {
     public static String getKakaoId(){
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        System.out.println(principal);
+        System.out.println("principal = " +principal);
         UserDetails userDetails = (UserDetails) principal;
         return userDetails.getUsername();
     }
