a3f13ddc41cf0443c87e967b2d4c5d5845a18c75
damongsanga <mike0413lata@gmail.com>
2024-03-31 23:19:56 +0900

    Hotfix: 리스트 관련 더미 데이터 입력

--- backend/src/main/java/com/ssafy/maryflower/bouquet/service/FlowerDataLoader.java
+++ backend/src/main/java/com/ssafy/maryflower/bouquet/service/FlowerDataLoader.java
@@ -8,14 +8,18 @@ import com.ssafy.maryflower.bouquet.data.repository.FlowerRepository;
 import com.ssafy.maryflower.member.data.entity.Member;
 import com.ssafy.maryflower.member.data.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.TimeZone;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class FlowerDataLoader implements CommandLineRunner {
 
     private final CacheService cacheService;
@@ -30,6 +34,7 @@ public class FlowerDataLoader implements CommandLineRunner {
         loadMemberData();
         loadBouquetData();
         cacheService.getAllFlowers();
+        initData();
     }
     private void loadFlowerData() {
         System.out.println("flower data load");
@@ -85,5 +90,47 @@ public class FlowerDataLoader implements CommandLineRunner {
         bouquetService.saveBucketData("동생", "생일","축하","testUrl",1L,testList);
 
 
+    }
+
+    private void initData() {
+
+        memberRepository.save(
+                Member.builder()
+                        .nickname("닉네임")
+                        .kakaoId("123")
+                        .build()
+        );
+
+        Member member1 = memberRepository.findByKakaoId("123").get();
+        Long memberId = member1.getMemberId();
+
+        loadFlowerData();
+
+        // 시간 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        String[] whoms = {"친구", "연인", "가족", "여자친구", "남자친구"};
+        String[] situations = {"졸업", "생일", "결혼", "시험", "기념일"};
+        String[] message = {"고생했어", "사랑해", "영원한 사랑", "귀여움", "소중함"};
+
+
+        for (int i = 0; i < 16; i++) {
+            bouquetService.saveBucketData(whoms[(int) (Math.random() * whoms.length)],
+                    situations[(int) (Math.random() * situations.length)],
+                    message[(int) (Math.random() * message.length)],
+                    "testUrl", memberId, pickThreeNumbers());
+        }
+        log.info("finished");
+    }
+
+    private static List<Long> pickThreeNumbers() {
+        // 1L부터 13L까지의 숫자를 저장할 리스트 생성
+        List<Long> numbers = new ArrayList<>();
+        for (long i = 1L; i <= 13L; i++) {
+            numbers.add(i);
+        }
+        // 리스트를 무작위로 섞음
+        Collections.shuffle(numbers);
+        // 섞인 리스트에서 상위 3개의 숫자를 선택
+        return numbers.subList(0, 3);
     }
 }
