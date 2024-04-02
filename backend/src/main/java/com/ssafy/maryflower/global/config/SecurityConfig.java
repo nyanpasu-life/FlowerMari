df93028b315001a106e5d5e8b0990b3f070a3ea5
damongsanga <mike0413lata@gmail.com>
2024-04-02 23:09:16 +0900

    Hotfix: Spring Security 재가동

--- backend/src/main/java/com/ssafy/maryflower/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/maryflower/global/config/SecurityConfig.java
@@ -46,13 +46,14 @@ public class SecurityConfig  {
             .sessionManagement(sm -> sm
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             // 테스트를 위해 authorize 기능 잠시 꺼둠
-//            .authorizeHttpRequests(authorize -> authorize
-//                    // 해당 API에 대해서는 모든 요청을 허가
-//                    .requestMatchers("/auth/oauth2/login/*", "/actuator/health").permitAll()
-//                    .anyRequest().authenticated())
             .authorizeHttpRequests(authorize -> authorize
-                    // 모든 요청을 허가
-                    .anyRequest().permitAll())
+                    // 해당 API에 대해서는 모든 요청을 허가
+                    .requestMatchers("/auth/oauth2/login/*", "/actuator/health").permitAll()
+                    .requestMatchers("/bouquet/subscribe").permitAll()
+                    .anyRequest().authenticated())
+//            .authorizeHttpRequests(authorize -> authorize
+//                    // 모든 요청을 허가
+//                    .anyRequest().permitAll())
 
             .exceptionHandling(ex -> {
               ex.authenticationEntryPoint(jwtAuthenticationEntryPoint)
